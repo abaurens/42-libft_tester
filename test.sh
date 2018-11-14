d29ed63e06417b11896f8c83fef0c4ad3d23d083
Arthur BAURENS <abaurens@student.42.fr>
2018-11-14 15:30:34 +0100

    libft_tester

--- test.sh
+++ test.sh
@@ -6,5 +6,14 @@ ln -s $LIB_PATH ./libft
 make re
 make clean
 ./lib_tester
+
+git clone "https://github.com/jgigault/42FileChecker.git" > /dev/null
+git clone "https://github.com/jtoty/Libftest.git" > /dev/null
+
+chmod 755 ./42FileChecker/42FileChecker.sh
+#./42FileChecker/42FileChecker.sh --project "libft"
+
 make fclean > /dev/null
 rm ./libft
+#rm -rf "42FileChecker"
+#rm -rf "Libftest"
